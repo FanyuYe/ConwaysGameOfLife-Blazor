c7bf6c5c70488155a9d2b959b1856ea11aab1461
Fanyu Ye <fanyu_ye@hotmail.com>
2021-05-07 23:39:37 +1000

    Add project files.

---
+++ ConwaysGameOfLife.App/Components/GameOfLifeCanvas.cs
@@ -0,0 +1,27 @@
+﻿using Blazor.Extensions;
+using Blazor.Extensions.Canvas.WebGL;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife.App.Components
+{
+    public partial class GameOfLifeCanvas
+    {
+        private WebGLContext ctx;
+        private BECanvasComponent canvas;
+
+        [Parameter]
+        public int Width { get; set; }
+
+        [Parameter]
+        public int Height { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            this.ctx = await this.canvas.CreateWebGLAsync();
+
+            await this.ctx.ClearColorAsync(0, 0, 0, 1);
+            await this.ctx.ClearAsync(BufferBits.COLOR_BUFFER_BIT);
+        }
+    }
+}
