9ec979cc30c7ee4acc99ced9ba58d26c086c43f2
Fanyu Ye <fanyu_ye@hotmail.com>
2021-05-13 00:55:24 +1000

    Implement GameController and add tests.

---
+++ ConwaysGameOfLife.Core.Tests/GameControllerTest.cs
@@ -0,0 +1,83 @@
+﻿using Moq;
+using System;
+using Xunit;
+
+namespace ConwaysGameOfLife.Core.Tests
+{
+    public class GameControllerTest
+    {
+        #region GameController(IWorld world, ISimulator simulator)
+
+        [Fact]
+        public void GameController_ParameterWorldIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new GameController(null, Mock.Of<ISimulator>()));
+        }
+
+        [Fact]
+        public void GameController_ParameterSimulatorIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new GameController(Mock.Of<IWorld>(), null));
+        }
+
+        #endregion
+
+        #region Reset()
+        
+        [Fact]
+        public void Reset_InvokeAfterModifyWorldState_WorldStateEqualToOriginState()
+        {
+            const int dimension = 2;
+            const int scale = 3;
+            const int size = scale * scale;
+            var expected = new bool[size];
+            var worldMock = new Mock<IWorld>()
+                .SetupProperty(world => world.Dimension, dimension)
+                .SetupProperty(world => world.Scale, scale)
+                .SetupProperty(world => world.State, (bool[])expected.Clone());
+            var world = worldMock.Object;
+            var sim = Mock.Of<ISimulator>();
+            var gameController = new GameController(world, sim);
+
+            for (int i = 0; i < world.State.Length; ++i)
+                world.State[i] = true;
+            gameController.Reset();
+
+            Assert.Equal<bool>(expected, world.State);
+        }
+
+        #endregion
+
+        #region Run(int)
+
+        [Fact]
+        public void Run_InvokeWithTicksParameterBeingZeroAndMinusOne_SimulatorNeverRuns()
+        {
+            var world = Mock.Of<IWorld>();
+            var simMock = new Mock<ISimulator>();
+            var gameController = new GameController(world, simMock.Object);
+            simMock.Setup(sim => sim.Tick(world));
+
+            gameController.Run(0);
+            gameController.Run(-1);
+
+            simMock.Verify(sim => sim.Tick(world), Times.Never);
+        }
+
+        [Fact]
+        public void Run_InvokeWithRandomTicksParameter_SimulatorRunsSameAmountOfTimes()
+        {
+            var world = Mock.Of<IWorld>();
+            var simMock = new Mock<ISimulator>();
+            var gameController = new GameController(world, simMock.Object);
+            simMock.Setup(sim => sim.Tick(world));
+            int times = new Random().Next(0, 10);
+
+            gameController.Run(times);
+
+            simMock.Verify(sim => sim.Tick(world), Times.Exactly(times));
+        }
+        
+        #endregion
+    }
+}
--- ConwaysGameOfLife.Core/GameController.cs
+++ ConwaysGameOfLife.Core/GameController.cs
@@ -4,8 +4,9 @@ namespace ConwaysGameOfLife.Core
 {
     public class GameController : IGameController
     {
-        private IWorld world;
-        private ISimulator simulator;
+        private readonly IWorld world;
+        private readonly ISimulator simulator;
+        private readonly bool[] seed;
 
         public GameController(IWorld world, ISimulator simulator)
         {
@@ -17,18 +18,20 @@ namespace ConwaysGameOfLife.Core
 
             this.world = world;
             this.simulator = simulator;
+            seed = (bool[])this.world.State.Clone();
         }
 
         /// <inheritdoc/>
         public void Reset()
         {
-            throw new NotImplementedException();
+            Buffer.BlockCopy(seed, 0, world.State, 0, Buffer.ByteLength(seed));
         }
 
         /// <inheritdoc/>
         public void Run(int ticks)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < ticks; ++i)
+                simulator.Tick(world);
         }
     }
 }
