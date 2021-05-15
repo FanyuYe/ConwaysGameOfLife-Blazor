21d208d0f91471edd3349c3fe3618ad083a20b6e
Fanyu Ye <fanyu_ye@hotmail.com>
2021-05-15 22:55:29 +1000

    Refactor UtilityTest.

--- ConwaysGameOfLife.Core.Tests/UtilityTest.cs
+++ ConwaysGameOfLife.Core.Tests/UtilityTest.cs
@@ -1,61 +1,32 @@
 using System;
-using System.Collections.Generic;
 using Xunit;
 
 namespace ConwaysGameOfLife.Core.Tests
 {
-    /// <summary>
-    /// TODO: 
-    /// 1. One Assert per test.
-    /// </summary>
     public class UtilityTest
     {
         #region Utility.ConvertCoordinateSingleToMulti
 
-        [Fact]
-        public void ConvertCoordinateSingleToMulti_ConvertTo1D_3_AllCorrect()
+        [Theory]
+        // 1D 3
+        [InlineData(0, new int[1] { 0 }, 3)]
+        [InlineData(1, new int[1] { 1 }, 3)]
+        [InlineData(2, new int[1] { 2 }, 3)]
+        // 2D 3*3
+        [InlineData(0, new int[2] { 0, 0 }, 3)]
+        [InlineData(1, new int[2] { 1, 0 }, 3)]
+        [InlineData(2, new int[2] { 2, 0 }, 3)]
+        [InlineData(3, new int[2] { 0, 1 }, 3)]
+        [InlineData(4, new int[2] { 1, 1 }, 3)]
+        [InlineData(5, new int[2] { 2, 1 }, 3)]
+        [InlineData(6, new int[2] { 0, 2 }, 3)]
+        [InlineData(7, new int[2] { 1, 2 }, 3)]
+        [InlineData(8, new int[2] { 2, 2 }, 3)]
+        public void ConvertCoordinateSingleToMulti_CorrectReturnValue(int originCoordinate, int[] expected, int scale)
         {
-            int dimension = 1;
-            int scale = 3;
-            int size = (int)Math.Pow(scale, dimension);
+            int[] actual = Utility.ConvertCoordinateSingleToMulti(expected.Length, scale, originCoordinate);
 
-            for (int i = 0; i < size; ++i)
-            {
-                int[] x = Utility.ConvertCoordinateSingleToMulti(dimension, scale, i);
-                var actual = x[0];
-                var expected = i;
-
-                Assert.Equal(expected, actual);
-            }
-        }
-
-        [Fact]
-        public void ConvertCoordinateSingleToMulti_ConvertTo2D_3x3_AllCorrect()
-        {
-            int dimension = 2;
-            int scale = 3;
-            int size = (int)Math.Pow(scale, dimension);
-            var reference = new Dictionary<int, int[]>()
-            {
-                { 0, new int[2] { 0, 0 } },
-                { 1, new int[2] { 1, 0 } },
-                { 2, new int[2] { 2, 0 } },
-                { 3, new int[2] { 0, 1 } },
-                { 4, new int[2] { 1, 1 } },
-                { 5, new int[2] { 2, 1 } },
-                { 6, new int[2] { 0, 2 } },
-                { 7, new int[2] { 1, 2 } },
-                { 8, new int[2] { 2, 2 } },
-            };
-
-            for (int i = 0; i < size; ++i)
-            {
-                int[] xy = Utility.ConvertCoordinateSingleToMulti(dimension, scale, i);
-                var actual = (xy[0], xy[1]);
-                var expected = (reference[i][0], reference[i][1]);
-
-                Assert.Equal(expected, actual);
-            }
+            Assert.Equal(expected, actual);
         }
 
         #endregion
@@ -74,48 +45,26 @@ namespace ConwaysGameOfLife.Core.Tests
             Assert.Throws<ArgumentNullException>(() => Utility.ConvertCoordinateMultiToSingle(3, null));
         }
 
-        [Fact]
-        public void ConvertCoordinateMultiToSingle_ConvertFrom1D_3_AllCorrect()
+        [Theory]
+        // 1D 3
+        [InlineData(new int[1] { 0 }, 0, 3)]
+        [InlineData(new int[1] { 1 }, 1, 3)]
+        [InlineData(new int[1] { 2 }, 2, 3)]
+        // 2D 3*3
+        [InlineData(new int[2] { 0, 0 }, 0, 3)]
+        [InlineData(new int[2] { 1, 0 }, 1, 3)]
+        [InlineData(new int[2] { 2, 0 }, 2, 3)]
+        [InlineData(new int[2] { 0, 1 }, 3, 3)]
+        [InlineData(new int[2] { 1, 1 }, 4, 3)]
+        [InlineData(new int[2] { 2, 1 }, 5, 3)]
+        [InlineData(new int[2] { 0, 2 }, 6, 3)]
+        [InlineData(new int[2] { 1, 2 }, 7, 3)]
+        [InlineData(new int[2] { 2, 2 }, 8, 3)]
+        public void ConvertCoordinateMultiToSingle_CorrectReturnValue(int[] originCoordinate, int expected, int scale)
         {
-            const int dimension = 1;
-            int scale = 3;
-            int size = (int)Math.Pow(scale, dimension);
+            int actual = Utility.ConvertCoordinateMultiToSingle(scale, originCoordinate);
 
-            for (int i = 0; i < size; ++i)
-            {
-                int actual = Utility.ConvertCoordinateMultiToSingle(scale, new int[dimension] { i });
-                var expected = i;
-
-                Assert.Equal(expected, actual);
-            }
-        }
-
-        [Fact]
-        public void ConvertCoordinateMultiToSingle_ConvertFrom2D_3x3_AllCorrect()
-        {
-            int dimension = 2;
-            int scale = 3;
-            int size = (int)Math.Pow(scale, dimension);
-            var reference = new Dictionary<int, int[]>()
-            {
-                { 0, new int[2] { 0, 0 } },
-                { 1, new int[2] { 1, 0 } },
-                { 2, new int[2] { 2, 0 } },
-                { 3, new int[2] { 0, 1 } },
-                { 4, new int[2] { 1, 1 } },
-                { 5, new int[2] { 2, 1 } },
-                { 6, new int[2] { 0, 2 } },
-                { 7, new int[2] { 1, 2 } },
-                { 8, new int[2] { 2, 2 } },
-            };
-
-            for (int i = 0; i < size; ++i)
-            {
-                int actual = Utility.ConvertCoordinateMultiToSingle(scale, reference[i]);
-                var expected = i;
-
-                Assert.Equal(expected, actual);
-            }
+            Assert.Equal(expected, actual);
         }
 
         #endregion
