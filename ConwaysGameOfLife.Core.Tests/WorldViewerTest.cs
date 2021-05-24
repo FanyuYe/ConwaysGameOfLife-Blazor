d36d49b766520d3f0e4408500f333bd4289a0a6c
Fanyu Ye <fanyu_ye@hotmail.com>
2021-05-24 22:00:31 +1000

    Add test for WorldViewer.

---
+++ ConwaysGameOfLife.Core.Tests/WorldViewerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace ConwaysGameOfLife.Core.Tests
+{
+    public class WorldViewerTest
+    {
+        #region Constructor
+
+        [Fact]
+        public void WorldViewer_ParameterWorldIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new WorldViewer(null));
+        }
+
+        #endregion
+    }
+}
