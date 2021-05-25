a05e31580883bd17e399e1e5e2aa1a8be4c14b46
Fanyu Ye <fanyu_ye@hotmail.com>
2021-05-25 21:57:17 +1000

    Proof of concept by drawing game using ASCII chars.

--- ConwaysGameOfLife.App/Components/GameOfLifeCanvas.cs
+++ ConwaysGameOfLife.App/Components/GameOfLifeCanvas.cs
@@ -1,27 +1,84 @@
 ﻿using Blazor.Extensions;
 using Blazor.Extensions.Canvas.WebGL;
+using ConwaysGameOfLife.Core;
 using Microsoft.AspNetCore.Components;
+using System;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ConwaysGameOfLife.App.Components
 {
     public partial class GameOfLifeCanvas
     {
-        private WebGLContext ctx;
-        private BECanvasComponent canvas;
+        private WebGLContext _ctx;
+        private BECanvasComponent _canvas;
+        private ConwaysGameOfLife2D _game;
 
         [Parameter]
-        public int Width { get; set; }
+        public int Width { get; set; } = 400;
 
         [Parameter]
-        public int Height { get; set; }
+        public int Height { get; set; } = 400;
+
+        [Parameter]
+        public int Scale { get; set; } = 10;
+
+        public int Iteration { get; private set; }
+
+        public string GameAsASCII { get; private set; }
+
+        private void Tick()
+        {
+            _game.Run();
+            Iteration++;
+            GameAsASCII = PrintGameASCII();
+        }
+
+        private string PrintGameASCII()
+        {
+            var sb = new StringBuilder();
+
+            for (int y = 0; y < _game.Scale; ++y)
+            {
+                for (int x = 0; x < _game.Scale; ++x)
+                {
+                    sb.Append(_game.GetState(x, y) ? 'X' : 'O');
+                }
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private void SetRandomSeed(double percentage)
+        {
+            Random rand = new Random(DateTime.UtcNow.Second);
+
+            for (int y = 0; y < _game.Scale; ++y)
+            {
+                for (int x = 0; x < _game.Scale; ++x)
+                {
+                    _game.SetState(x, y, rand.NextDouble() <= percentage);
+                }
+            }
+        }
+
+        protected override void OnParametersSet()
+        {
+            _game = ConwaysGameOfLife2D.CreateClassicGame(Scale);
+            SetRandomSeed(0.5);
+            GameAsASCII = PrintGameASCII();
+
+            base.OnParametersSet();
+        }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            this.ctx = await this.canvas.CreateWebGLAsync();
+            _ctx = await _canvas.CreateWebGLAsync();
 
-            await this.ctx.ClearColorAsync(0, 0, 0, 1);
-            await this.ctx.ClearAsync(BufferBits.COLOR_BUFFER_BIT);
+            await _ctx.ClearColorAsync(0, 0, 0, 1);
+            await _ctx.ClearAsync(BufferBits.COLOR_BUFFER_BIT);
         }
     }
 }
