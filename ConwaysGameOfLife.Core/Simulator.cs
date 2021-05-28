aabdaf965f79e46993bc869339dbd57261a00851
Fanyu Ye <fanyu_ye@hotmail.com>
2021-05-28 21:53:37 +1000

    Avoid repeatedly memory allocation.

--- ConwaysGameOfLife.Core/Simulator.cs
+++ ConwaysGameOfLife.Core/Simulator.cs
@@ -10,6 +10,7 @@ namespace ConwaysGameOfLife.Core
     {
         private readonly IWorld _world;
         private readonly IRule _rule;
+        private readonly bool[] _nextState;
 
         /// <summary>
         /// Constructor.
@@ -22,19 +23,19 @@ namespace ConwaysGameOfLife.Core
 
             _rule = rule
                 ?? throw new ArgumentNullException(nameof(rule));
+
+            _nextState = new bool[_world.State.Length];
         }
 
         /// <inheritdoc/>
         public void Tick()
         {
-            var nextState = new bool[_world.State.Length];
-
-            Parallel.For(0, nextState.Length, i => 
+            Parallel.For(0, _nextState.Length, i => 
             {
-                nextState[i] = _rule.GetNextIterationOfCell(_world, i);
+                _nextState[i] = _rule.GetNextIterationOfCell(_world, i);
             });
 
-            Buffer.BlockCopy(nextState, 0, _world.State, 0, Buffer.ByteLength(nextState));
+            Buffer.BlockCopy(_nextState, 0, _world.State, 0, Buffer.ByteLength(_nextState));
         }
     }
 }
